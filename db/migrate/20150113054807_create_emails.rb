fe9cca7f2bab1371bc86483aa17e843d2e9f09b4
rasg <rodolfo@mutalis.com>
2015-01-13 00:00:26 -0600

    Email resource scaffold added.

---
+++ db/migrate/20150113054807_create_emails.rb
@@ -0,0 +1,11 @@
+class CreateEmails < ActiveRecord::Migration
+  def change
+    create_table :emails do |t|
+      t.string :username
+      t.string :quota
+      t.string :password
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/emails.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :email do
+    username "MyString"
+quota "MyString"
+password "MyString"
+  end
+
+end
