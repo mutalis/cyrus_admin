1c4f30f562158bbc412b7fcb5a4ba9eeee4dead4
rasg <rodolfo@mutalis.com>
2015-02-09 23:24:47 -0600

    Unit tests added for the Domain model.

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,2 +1,5 @@
 class Domain < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: { case_sensitive: false },
+            format: { with: /\A[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,13}\z/ }
+  validates :quota, numericality: { only_integer: true }
 end
--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Domain, :type => :model do
 
+  it "has a valid factory" do
+     expect(build(:domain)).to be_valid
+  end
+   
+  subject(:domain) { FactoryGirl.create :domain }
+  let(:email) { FactoryGirl.create(:email) }
+
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  it { expect(domain.name).to match(/\A[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,13}\z/) }
+
+  it { is_expected.to validate_numericality_of :quota }
+  
 end
