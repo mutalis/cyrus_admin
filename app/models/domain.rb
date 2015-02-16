71745352709c43ed96082e0eeed1483c8e8b60ca
rasg <rodolfo@mutalis.com>
2015-02-15 18:37:47 -0600

    The domain quota validation added. 0 <= domain_quota <=1000. unit test included.

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,5 +2,5 @@ class Domain < ActiveRecord::Base
   has_many :emails, inverse_of: :domain
   validates :name, presence: true, length: { maximum: 64 }, uniqueness: { case_sensitive: false },
             format: { with: /\A[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,13}\z/ }
-  validates :quota, numericality: { only_integer: true }
+  validates :quota, numericality: { only_integer: true , greater_than_or_equal_to: 1, less_than_or_equal_to: 1000 }
 end
--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -14,8 +14,10 @@ RSpec.describe Domain, :type => :model do
   it { expect(domain.name).to match(/\A[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,13}\z/) }
   it { is_expected.to validate_length_of(:name).is_at_most(64) }
 
-  it { is_expected.to validate_numericality_of :quota }
-
+  it { is_expected.to validate_numericality_of(:quota).only_integer }
+  it { is_expected.to validate_numericality_of(:quota).is_greater_than_or_equal_to(1) }
+  it { is_expected.to validate_numericality_of(:quota).is_less_than_or_equal_to(1000) }
+  
   it { is_expected.to have_many(:emails).inverse_of(:domain) }
 
   it { is_expected.to have_db_index(:name) }
