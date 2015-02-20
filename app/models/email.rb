1347165f5da44023e7f441914a17154687e6d2fc
rasg <rodolfo@mutalis.com>
2015-02-20 15:07:10 -0600

    Validate email password and set_email_address method added.

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,12 +1,23 @@
 class Email < ActiveRecord::Base
   belongs_to :domain, inverse_of: :emails
   attr_accessor :confirmation_password
-  # validates :password, length: { minimum: 8 }
-  validates :username, presence: true, length: { maximum: 40 },
+  
+  before_validation :set_email_address, on: :create
+  
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :username, presence: true, length: { maximum: 104 },
             format: { with: /\A[a-zA-Z0-9]+([_\.\-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,13}\z/ },
             uniqueness: { case_sensitive: false }
 
   validates :quota, numericality: { only_integer: true }
 
   validates :domain, presence: true
+  
+  default_scope { order(username: :asc) }
+
+  protected
+  
+  def set_email_address
+    self.username += '@' + domain.name
+  end
 end
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -9,10 +9,13 @@ RSpec.describe Email, :type => :model do
   it { is_expected.to validate_presence_of :username }
   it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
   it { expect(email.username).to match(/\A[a-zA-Z0-9]+([_\.\-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,13}\z/) }
-  it { is_expected.to validate_length_of(:username).is_at_most(40) }
+  it { is_expected.to validate_length_of(:username).is_at_most(104) }
 
   it { is_expected.to belong_to(:domain).inverse_of(:emails) }
   it { is_expected.to validate_presence_of :domain }
   
   it { is_expected.to have_db_index(:username) }
+  
+  it { is_expected.to validate_presence_of :password }
+  it { is_expected.to validate_length_of(:password).is_at_least(8) }
 end
