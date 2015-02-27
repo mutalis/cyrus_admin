0ffa69489551e47ff47e5a41d369b4c94b3e3292
rasg <rodolfo@mutalis.com>
2015-02-27 14:45:35 -0600

    Testing setup: DatabaseCleaner Rspec configuration changed because some failures with tests related to ActiveRecord. Using the configuration defined in the DatabaseCleaner docs .

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,10 +41,6 @@ RSpec.configure do |config|
      end
    end
 
-   config.after(:each) do
-     DatabaseCleaner.clean
-   end
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
